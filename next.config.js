3f3bf410cc30a0bd39ea73e94b6c19f4cb9fb49b
chidananda <chidananda@iterator.in>
2023-10-06 13:15:22 +0530

    connected wagtail cms

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "source.unsplash.com",
+      "images.unsplash.com",
+      "randomuser.me",
+      "localhost",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,209 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import axios from "axios";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={`xl:pt-0 sm:p-2 ${inter.className}`}>
+      <section className="dark:text-gray-100">
+        <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+          <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            {data?.items?.map((item, index) => (
+              <Link
+                href={`/blogs/${item?.id}`}
+                key={index}
+                rel="noopener noreferrer"
+                className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900"
+              >
+                <Image
+                  width={100}
+                  height={100}
+                  alt="test"
+                  role="presentation"
+                  className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                  src="https://source.unsplash.com/random/480x360?1"
+                />
+                <div className="p-6 space-y-2">
+                  <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                    {item.title}
+                  </h3>
+                  <span className="text-xs dark:text-gray-400">
+                    January 21, 2021
+                  </span>
+                  <p>
+                    Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                    neglegentur, ex has tantas percipit perfecto. At per tempor
+                    albucius perfecto, ei probatus consulatu patrioque mea, ei
+                    vocent delicata indoctum pri.
+                  </p>
+                </div>
+              </Link>
+            ))}
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900"
+            >
+              <Image
+                height={100}
+                alt=""
+                width={100}
+                // role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://source.unsplash.com/random/480x360?2"
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  In usu laoreet repudiare legendos
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  January 22, 2021
+                </span>
+                <p>
+                  Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                  neglegentur, ex has tantas percipit perfecto. At per tempor
+                  albucius perfecto, ei probatus consulatu patrioque mea, ei
+                  vocent delicata indoctum pri.
+                </p>
+              </div>
+            </a>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900"
+            >
+              <Image
+                height={100}
+                width={100}
+                alt=""
+                // role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://source.unsplash.com/random/480x360?3"
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  In usu laoreet repudiare legendos
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  January 23, 2021
+                </span>
+                <p>
+                  Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                  neglegentur, ex has tantas percipit perfecto. At per tempor
+                  albucius perfecto, ei probatus consulatu patrioque mea, ei
+                  vocent delicata indoctum pri.
+                </p>
+              </div>
+            </a>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block"
+            >
+              <Image
+                alt=""
+                width={100}
+                height={100}
+                // role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://source.unsplash.com/random/480x360?4"
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  In usu laoreet repudiare legendos
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  January 24, 2021
+                </span>
+                <p>
+                  Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                  neglegentur, ex has tantas percipit perfecto. At per tempor
+                  albucius perfecto, ei probatus consulatu patrioque mea, ei
+                  vocent delicata indoctum pri.
+                </p>
+              </div>
+            </a>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block"
+            >
+              <Image
+                alt=""
+                width={100}
+                height={100}
+                role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://source.unsplash.com/random/480x360?5"
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  In usu laoreet repudiare legendos
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  January 25, 2021
+                </span>
+                <p>
+                  Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                  neglegentur, ex has tantas percipit perfecto. At per tempor
+                  albucius perfecto, ei probatus consulatu patrioque mea, ei
+                  vocent delicata indoctum pri.
+                </p>
+              </div>
+            </a>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block"
+            >
+              <Image
+                height={100}
+                alt=""
+                width={100}
+                role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://source.unsplash.com/random/480x360?6"
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  In usu laoreet repudiare legendos
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  January 26, 2021
+                </span>
+                <p>
+                  Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                  neglegentur, ex has tantas percipit perfecto. At per tempor
+                  albucius perfecto, ei probatus consulatu patrioque mea, ei
+                  vocent delicata indoctum pri.
+                </p>
+              </div>
+            </a>
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="button"
+              className="px-6 py-3 text-sm rounded-md hover:underline dark:bg-gray-900 dark:text-gray-400"
+            >
+              Load more posts...
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get(`http://127.0.0.1:8000/api/v2/pages/`);
+
+  return {
+    props: {
+      data: res?.data,
+    },
+  };
 }
